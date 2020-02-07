c4f45aad613f064458bf077cfef4ebb3fda8d966
pfg <omegariisthebest@gmail.com>
2020-02-07 09:51:21 -0800

    format code

--- mapexplorer.js
+++ mapexplorer.js
@@ -25,27 +25,23 @@ function rerender() {
 
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-		ctx.textBaseline = "top";
-	ctx.font = characterWidth+"px monospace";
+  ctx.textBaseline = "top";
+  ctx.font = characterWidth + "px monospace";
 
   for (let y = yst; y <= cy; y++) {
     for (let x = xst; x <= cx; x++) {
-	    ctx.fillStyle = "black"; // replace this with the real color
-	    let xUL = x * characterWidth - drawOffsetX;
-	    let yUL = y * characterHeight - drawOffsetY;
-	    if(x === gmxCoord && y === gmyCoord){
-		   // console.log(xUL, yUL);
-ctx.fillRect(xUL, yUL, characterWidth, characterHeight);
-		    ctx.fillStyle = "white";
-	    }
-      ctx.fillText(
-        generateWorldTileAt(x, y),
-        xUL,
-        yUL,
-      );
+      ctx.fillStyle = "black"; // replace this with the real color
+      let xUL = x * characterWidth - drawOffsetX;
+      let yUL = y * characterHeight - drawOffsetY;
+      if (x === gmxCoord && y === gmyCoord) {
+        // console.log(xUL, yUL);
+        ctx.fillRect(xUL, yUL, characterWidth, characterHeight);
+        ctx.fillStyle = "white";
+      }
+      ctx.fillText(generateWorldTileAt(x, y), xUL, yUL);
     }
   }
-	ctx.fillText(`Coords: ${gmxCoord}/${gmyCoord}`, 10, 10);
+  ctx.fillText(`Coords: ${gmxCoord}/${gmyCoord}`, 10, 10);
 }
 
 function updateBoardSize() {
@@ -67,47 +63,49 @@ document.body.appendChild(board);
 let pinx = 0;
 let piny = 0;
 let mousedown = false;
-document.addEventListener("mousedown", (e) => {
-	mousedown = true;
-	pinx = e.clientX;
-	piny = e.clientY;
+document.addEventListener("mousedown", e => {
+  mousedown = true;
+  pinx = e.clientX;
+  piny = e.clientY;
 });
 
-document.addEventListener("mousemove", (e) => {
-	if(mousedown){
-		drawOffsetX += pinx - e.clientX;
-		pinx = e.clientX;
-		drawOffsetY += piny - e.clientY;
-		piny = e.clientY;
-		rerender();
-	}
+document.addEventListener("mousemove", e => {
+  if (mousedown) {
+    drawOffsetX += pinx - e.clientX;
+    pinx = e.clientX;
+    drawOffsetY += piny - e.clientY;
+    piny = e.clientY;
+    rerender();
+  }
 
-	let mouseXCoord = Math.round((e.clientX + drawOffsetX) / characterWidth);
-	let mouseYCoord = Math.round((e.clientY + drawOffsetY) / characterHeight);
+  let mouseXCoord = Math.round((e.clientX + drawOffsetX) / characterWidth);
+  let mouseYCoord = Math.round((e.clientY + drawOffsetY) / characterHeight);
 
-		gmxCoord = mouseXCoord;
-		gmyCoord = mouseYCoord;
-		rerender();
+  gmxCoord = mouseXCoord;
+  gmyCoord = mouseYCoord;
+  rerender();
 });
 
 document.addEventListener("mouseup", e => {
-	mousedown = false;
-
+  mousedown = false;
 });
 document.addEventListener("wheel", e => {
-	let dy = e.deltaY;
-	let ow = characterWidth;
-	let oh = characterHeight;
-	let mx = e.clientX;
-	let my = e.clientY;
-	characterWidth -= dy;
-	characterHeight -= dy;
-	if(characterWidth < 10){
-	characterWidth = 10; characterHeight = 10;}
+  let dy = e.deltaY;
+  let ow = characterWidth;
+  let oh = characterHeight;
+  let mx = e.clientX;
+  let my = e.clientY;
+  characterWidth -= dy;
+  characterHeight -= dy;
+  if (characterWidth < 10) {
+    characterWidth = 10;
+    characterHeight = 10;
+  }
 
+  drawOffsetX -=
+    gmxCoord * ow - drawOffsetX - (gmxCoord * characterWidth - drawOffsetX);
+  drawOffsetY -=
+    gmyCoord * oh - drawOffsetY - (gmyCoord * characterHeight - drawOffsetY);
 
-drawOffsetX -= (gmxCoord * ow - drawOffsetX) - (gmxCoord * characterWidth - drawOffsetX);
-	drawOffsetY -= (gmyCoord * oh - drawOffsetY) - (gmyCoord * characterHeight - drawOffsetY);
-
-	rerender();
+  rerender();
 });
