ab38089b439c0c43b3d496d7225efcb3fe4099e7
pfg <omegariisthebest@gmail.com>
2020-02-07 15:38:04 -0800

    mobile support when it feels like it. on ios it works best by selecting share > add to home screen

--- mapexplorer.js
+++ mapexplorer.js
@@ -12,13 +12,20 @@ let PIXELRATIO = 1;
 
 let characterWidth = 30;
 let characterHeight = 30;
-let drawOffsetX = 25;
-let drawOffsetY = 25;
+let drawOffsetX = -150;
+let drawOffsetY = -150;
 
 let gmxCoord = 0;
 let gmyCoord = 0;
 
+let rerenderTriggered = false;
 function rerender() {
+  if (rerenderTriggered) return;
+  rerenderTriggered = true;
+  window.requestAnimationFrame(rerenderNow);
+}
+function rerenderNow() {
+  rerenderTriggered = false;
   let xst = Math.floor(drawOffsetX / characterWidth);
   let yst = Math.floor(drawOffsetY / characterHeight);
 
@@ -47,10 +54,10 @@ function rerender() {
       ctx.fillText(generateWorldTileAt(x, y), xUL, yUL);
     }
   }
-  ctx.font = "16pt sans-serif";
+  ctx.font = "12pt sans-serif";
   ctx.textBaseline = "top";
   let targetTile = generateWorldTileAt(gmxCoord, gmyCoord);
-  let text = `Coords: ${gmxCoord}x/${gmyCoord}y. Targeted tile: ${window.inverseTiles[targetTile]}`;
+  let text = `Coords: ${gmxCoord}x/${gmyCoord}y. Selected Tile: ${window.inverseTiles[targetTile]}`;
   let textSize = ctx.measureText(text);
   let textHeight = ctx.measureText("@").width; // approx. see https://stackoverflow.com/questions/1134586/how-can-you-find-the-height-of-text-on-an-html-canvas
   ctx.fillStyle = "white";
@@ -85,66 +92,99 @@ window.addEventListener("resize", () => {
 document.body.appendChild(board);
 
 let pointers = {};
+let touchPointers = [];
+let scalev = undefined;
+
+function cursorPosToBoardCoordExact(x, y) {
+  let mouseXCoord = (x + drawOffsetX) / characterWidth;
+  let mouseYCoord = (y + drawOffsetY) / characterHeight;
+  return [mouseXCoord, mouseYCoord];
+}
+
+function cursorPosToBoardCoord(x, y) {
+  let [mouseXCoord, mouseYCoord] = cursorPosToBoardCoordExact(x, y);
+  return [Math.floor(mouseXCoord), Math.floor(mouseYCoord)];
+}
 
 document.addEventListener("pointerdown", e => {
   pointers[e.pointerId] = {
     mousedown: true,
     pinx: e.clientX,
     piny: e.clientY,
-    event: e
+    event: e,
+    id: e.pointerId
   };
+  if (e.pointerType === "touch") {
+    scalev = undefined;
+    touchPointers.push(pointers[e.pointerId]);
+  }
 });
 
 document.addEventListener("pointermove", e => {
   let pt = pointers[e.pointerId];
   if (pt && pt.mousedown) {
-    drawOffsetX += pt.pinx - e.clientX;
-    pt.pinx = e.clientX;
-    drawOffsetY += pt.piny - e.clientY;
-    pt.piny = e.clientY;
+    pt.didMove = true;
+    if (touchPointers.length === 2) {
+      pt.pinx = e.clientX;
+      pt.piny = e.clientY;
+      let [p1, p2] = touchPointers;
+      let d = Math.sqrt((p2.pinx - p1.pinx) ** 2 + (p2.piny - p1.piny) ** 2);
+      if (!scalev) scalev = d;
+      let offset = d - scalev;
+      scalev = d;
+      let factor = -(offset / 10);
+      let [cx, cy] = [
+        (p1.pinx - p2.pinx) / 2 + p2.pinx,
+        (p1.piny - p2.piny) / 2 + p2.piny
+      ];
+      let [centerX, centerY] = cursorPosToBoardCoordExact(cx, cy);
+      scale(factor, centerX, centerY);
+      // alert(factor);
+    } else {
+      drawOffsetX += pt.pinx - e.clientX;
+      pt.pinx = e.clientX;
+      drawOffsetY += pt.piny - e.clientY;
+      pt.piny = e.clientY;
+    }
+
     rerender();
   }
-
-  let mouseXCoord = Math.floor((e.clientX + drawOffsetX) / characterWidth);
-  let mouseYCoord = Math.floor((e.clientY + drawOffsetY) / characterHeight);
-
-  gmxCoord = mouseXCoord;
-  gmyCoord = mouseYCoord;
-  rerender();
 });
 
 document.addEventListener("pointerup", e => {
   let pt = pointers[e.pointerId];
   if (pt) pt.mousedown = false;
   delete pointers[e.pointerId];
+  touchPointers = touchPointers.filter(tp => tp.id !== e.pointerId);
+
+  if (pt && !pt.didMove) {
+    [gmxCoord, gmyCoord] = cursorPosToBoardCoord(e.clientX, e.clientY);
+    rerender();
+  }
 });
-document.addEventListener("wheel", e => {
-  let dy = e.deltaY;
+
+function scale(factor, centerX, centerY) {
   let ow = characterWidth;
   let oh = characterHeight;
-  let mx = e.clientX;
-  let my = e.clientY;
-  characterWidth -= dy;
-  characterHeight -= dy;
+  characterWidth -= factor;
+  characterHeight -= factor;
   if (characterWidth < 10) {
     characterWidth = 10;
     characterHeight = 10;
   }
 
   drawOffsetX -=
-    gmxCoord * ow - drawOffsetX - (gmxCoord * characterWidth - drawOffsetX);
+    centerX * ow - drawOffsetX - (centerX * characterWidth - drawOffsetX);
   drawOffsetY -=
-    gmyCoord * oh - drawOffsetY - (gmyCoord * characterHeight - drawOffsetY);
+    centerY * oh - drawOffsetY - (centerY * characterHeight - drawOffsetY);
 
   rerender();
-});
+}
 
-document.addEventListener(
-  "touchstart",
-  event => {
-    if (event.touches.length > 1) {
-      event.preventDefault();
-    }
-  },
-  { passive: false, capture: true }
-);
+document.addEventListener("wheel", e => {
+  let [centerX, centerY] = cursorPosToBoardCoordExact(e.clientX, e.clientY);
+
+  let dy = e.deltaY;
+
+  scale(dy, centerX, centerY);
+});
